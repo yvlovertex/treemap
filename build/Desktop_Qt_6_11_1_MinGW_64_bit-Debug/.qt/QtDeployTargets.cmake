51c164ef15c651d0a4fa0e0df278dd42751e1271
yvlovertex <oz.overtex@gmail.com>
2026-07-29 13:21:52 +0200

    cleanup

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_treemap_FILE C:/Users/PC/Desktop/treemap/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/treemap.exe)
-set(__QT_DEPLOY_TARGET_treemap_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_treemap_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/treemap.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for treemap.
-# This may be replaced when dependencies are built.
